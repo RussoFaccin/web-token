90f1390e634a1a66a87f1e495fc7ce09e0ab1b66
Rodrigo Russo <rodrigorusso@fernando-campos.seculoxxi.com>
2017-03-20 09:20:29 -0300

    200317

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,20 @@
 var CryptoJS = require("crypto-js");
-var SHA256 = require("crypto-js/sha256");
 var fld_user = document.getElementById('fld_user');
 var fld_mail = document.getElementById('fld_mail');
-var submit_btn = document.getElementById('btn_create');
+var btn_submit = document.getElementById('btn_create');
+var btn_verify = document.getElementById('btn_verify');
+btn_verify.style.display = "none";
 var message_box = document.getElementById('message');
-var SECRET = "123456";
+var SECRET = "EtCngPx6";
 
-submit_btn.onclick = function(){
+btn_submit.onclick = function(){
   var user_name = fld_user.value;
   var user_mail = fld_mail.value;
-  createToken(user_name, user_mail);
+  var token = createToken(user_name, user_mail);
+  btn_verify.style.display = "block";
+  btn_verify.onclick = function(){
+    verifyToken(token);
+  }
 };
 
 function createToken(name, mail, isadmin=true){
@@ -19,7 +24,20 @@ function createToken(name, mail, isadmin=true){
     admin: isadmin
   };
   var hash_payload = window.btoa(JSON.stringify(user));
-  var hash_signature = SHA256(hash_payload, SECRET);
+  var hash_signature = CryptoJS.HmacSHA256(hash_payload, SECRET).toString();
   var web_token = hash_payload +"."+ hash_signature;
-  console.log("web_token: "+web_token);
+  message_box.innerHTML = web_token;
+  return web_token;
+}
+
+function verifyToken(token) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log("RESPONSE: "+this.responseText);
+        }
+    };
+    xhttp.open("POST", "./auth/verifyuser.php", true);
+    xhttp.setRequestHeader("Authorization", token);
+    xhttp.send();
 }
